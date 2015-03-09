f4a2b8c4c915591b51e0693c82f980305933282e
Dave Strus <dave@davestrus.com>
2015-03-09 17:02:08 -0400

    Append new task to the <ol> when form is submitted.

--- js/listly.js
+++ js/listly.js
@@ -1,12 +1,23 @@
-function Listly() {
+var Listly = function() {
 
   function Listly() {
     this.tasks = [];
-
     var self = this;
+
+    $('form#new_task').on('submit', function(ev) {
+      ev.preventDefault();
+      var task_name = this.task_name.value;
+
+      // Add a list item to #tasks
+      var result = $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+      this.task_name.value = '';
+      $(this.task_name).focus();
+
+      // Add the task name to tasks array
+    });
   }
-  
+
   return Listly;
-}
+}();
 
 var listly = new Listly();
