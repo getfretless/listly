761213590b19644308bf0a802d734c8e78af32f9
Dave Strus <dave@davestrus.com>
2015-03-10 17:28:17 -0400

    Create Task model.

---
+++ js/task.js
@@ -0,0 +1,25 @@
+var Task = function() {
+  var self = this;
+  self.counter = 1;
+
+  function getOrSetId(id) {
+    if (!id) {
+      id = self.counter + 1;
+    }
+    incrementCounter(id);
+    return id;
+  }
+
+  function incrementCounter(id) {
+    if (id > self.counter) {
+      self.counter = id;
+    }
+  }
+
+  function Task(properties) {
+    this.name = properties.name;
+    this.id = getOrSetId(properties.id);
+  }
+
+  return Task;
+}();
