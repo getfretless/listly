0572dac4fe0da6866e8a455417bb59bbdcb4a2f2
Dave Strus <dave@davestrus.com>
2015-03-10 12:31:25 -0400

    Remove list item when delete button is pressed. Show error message when localStorage access fails.

--- js/listly.js
+++ js/listly.js
@@ -7,7 +7,7 @@ var Listly = function() {
     function addTask(task_name) {
       self.tasks.push(task_name);
       if (save()) {
-        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+        appendToList(task_name);
         return true;
       }
       else {
@@ -15,18 +15,61 @@ var Listly = function() {
       }
     }
 
-    function load() {
-      self.tasks = JSON.parse(localStorage.tasks);
-      $.each(self.tasks, function(index, task_name) {
-        $('#tasks').append('<li class="list-group-item">' + task_name + '</li>');
+    function appendToList(task_name) {
+      // Grab a copy of the list item template.
+      var li = $('#list_item_template').clone();
+      li.removeAttr('id');
+
+      // Add the task name to the LI's label.
+      li.find('label').text(task_name);
+
+      // Unhide the new LI.
+      li.removeClass('hidden');
+
+      // Activate the delete button.
+      li.find('.btn-danger').click(function() {
+        // Remove it from the array
+
+
+        // Save the array to local storage.
+
+        // Remove it from the <ol>.
+        li.remove();
       });
+
+      $('#tasks').append(li);
+    }
+
+    function showFormError(form) {
+      // add message inside alert div
+      $(form).find('.alert')
+        .html('Aww, <em>cuss</em>! Something went wrong')
+        .removeClass('hidden');
+    }
+
+    function supportsLocalStorage() {
+      try {
+         return 'localStorage' in window && window.localStorage !== null;
+      }
+      catch(err) {
+        return false;
+      }
+    }
+
+    function load() {
+      if (supportsLocalStorage() && localStorage.tasks) {
+        self.tasks = JSON.parse(localStorage.tasks);
+        $.each(self.tasks, function(index, task_name) {
+          appendToList(task_name);
+        });
+      }
     }
 
     function save() {
-      try {
+      if (supportsLocalStorage()) {
         return (localStorage.tasks = JSON.stringify(self.tasks));
       }
-      catch(err) {
+      else {
         return false;
       }
     }
@@ -40,6 +83,9 @@ var Listly = function() {
 
       if (addTask(task_name)) {
         field.val('');
+      }
+      else {
+        showFormError(this);
       }
       field.focus().select();
     });
