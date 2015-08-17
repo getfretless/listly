70788dd65c50490ab571cd700c843b333d9dc321
Dave Strus <dave@davestrus.com>
2015-08-17 14:05:21 -0400

    Update to use RequireJS.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+requirejs([
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js',
+  'js/task.js',
+  'js/listly.js'
+]);
